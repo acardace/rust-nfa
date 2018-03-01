263eba26ec4f45867a4670a9fd9c300de004eada
Antonio Cardace <anto.cardace@gmail.com>
2018-03-01 16:09:02 +0000

    main: parse RE from cmdline

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 extern crate nfa;
 
 use nfa::algorithms::*;
+use std::env::*;
 
 fn main() {
-    let re = "a|bc*";
-    let nfa = re_to_nfa(&re);
-    println!("{:?}", nfa);
+    if let Some(re) = args().nth(1) {
+        match re_to_nfa(&re) {
+            Ok(nfa) => println!("{:?}", nfa),
+            Err(s) => println!("{}", s),
+        }
+    } else {
+        println!("Usage: {:?} RE", current_exe().unwrap().file_name().unwrap());
+    }
 }
