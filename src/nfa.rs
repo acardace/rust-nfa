ebcbb202cad5338f4831150caf812f4c9e44c9e1
Antonio Cardace <anto.cardace@gmail.com>
2018-03-01 01:13:31 +0000

    nfa: add checks for empty NFA

--- src/nfa.rs
+++ src/nfa.rs
@@ -23,62 +23,69 @@ impl Nfa {
 
     pub fn concatenate(&mut self, operand: &Nfa) {
         // add middle ε transition and vertex
-        let last_vertex = *self.vertexes.last().unwrap();
-        self.vertexes.push(last_vertex + 1);
-        self.transitions.push((last_vertex, 'ε', last_vertex + 1));
-
-        // concat vertexes
-        let last_vertex = *self.vertexes.last().unwrap();
-        for vertex in operand.vertexes.iter() {
-            self.vertexes.push(*vertex + last_vertex + 1);
-        }
-        // concat nfa copying transition function
-        for &(s, c, e) in operand.transitions.iter() {
-            self.transitions.push((s + last_vertex + 1, c, e + last_vertex + 1));
+        if self.vertexes.len() > 0 {
+            let self_final_state = *self.vertexes.last().unwrap();
+            // concat vertexes
+            for vertex in operand.vertexes.iter() {
+                self.vertexes.push(*vertex + self_final_state + 1);
+            }
+            // add ε move
+            self.transitions.push((self_final_state, 'ε', operand.vertexes[0] + self_final_state + 1));
+            // concat nfa copying transition function
+            for &(s, c, e) in operand.transitions.iter() {
+                self.transitions.push((s + self_final_state + 1, c, e + self_final_state + 1));
+            }
+        } else {
+            // if empty just clone
+            *self = operand.clone();
         }
     }
 
     pub fn kleene(&mut self) {
-        let start_state = self.vertexes[0];
-        let final_state = *self.vertexes.last().unwrap();
-        self.transitions.push((final_state, 'ε', start_state));
+        if self.vertexes.len() > 0 {
+            let start_state = self.vertexes[0];
+            let final_state = *self.vertexes.last().unwrap();
+            self.transitions.push((final_state, 'ε', start_state));
 
-        self.shift_right(); 
-        let start_state = self.vertexes[0];
-        self.vertexes.insert(0, start_state - 1);
-        self.transitions.push((start_state - 1, 'ε', start_state));
+            self.shift_right(); 
+            let start_state = self.vertexes[0];
+            self.vertexes.insert(0, start_state - 1);
+            self.transitions.push((start_state - 1, 'ε', start_state));
 
-        let start_state = self.vertexes[0];
-        let final_state = *self.vertexes.last().unwrap();
-        self.vertexes.push(final_state + 1);
-        self.transitions.push((start_state, 'ε', final_state + 1));
-        self.transitions.push((final_state, 'ε', final_state + 1));
+            let start_state = self.vertexes[0];
+            let final_state = *self.vertexes.last().unwrap();
+            self.vertexes.push(final_state + 1);
+            self.transitions.push((start_state, 'ε', final_state + 1));
+            self.transitions.push((final_state, 'ε', final_state + 1));
+        }
     }
 
     pub fn or(&mut self, operand: &Nfa) {
-        self.shift_right(); 
-        // add new initial state
-        let self_start_state = self.vertexes[0];
-        self.vertexes.insert(0, self_start_state - 1);
-        // add ε moves
-        self.transitions.push((0, 'ε', self.vertexes[1]));
-        let self_final_state = *self.vertexes.last().unwrap();
-        // add second NFA
-        let last_vertex = self_final_state + 1;
-        for v in operand.vertexes.iter() {
-            self.vertexes.push(*v + last_vertex);
+        if self.vertexes.len() > 0 {
+            self.shift_right(); 
+            // add new initial state
+            let self_start_state = self.vertexes[0];
+            self.vertexes.insert(0, self_start_state - 1);
+            // add ε moves
+            self.transitions.push((0, 'ε', self.vertexes[1]));
+            let self_final_state = *self.vertexes.last().unwrap();
+            // add second NFA
+            let last_vertex = self_final_state + 1;
+            for v in operand.vertexes.iter() {
+                self.vertexes.push(*v + last_vertex);
+            }
+            for tran in operand.transitions.iter() {
+                self.transitions.push((tran.0 + last_vertex, tran.1, tran.2 + last_vertex));
+            }
+            // add ε move
+            self.transitions.push((0, 'ε', operand.vertexes[0] + last_vertex));
+            let op_final_state = *self.vertexes.last().unwrap();
+            // add final state
+            let nfa_final_state = op_final_state + 1;
+            self.vertexes.push(nfa_final_state);
+            self.transitions.push((self_final_state, 'ε', nfa_final_state));
+            self.transitions.push((op_final_state, 'ε', nfa_final_state));
         }
-        for tran in operand.transitions.iter() {
-            self.transitions.push((tran.0 + last_vertex, tran.1, tran.2 + last_vertex));
-        }
-        // add ε move
-        self.transitions.push((0, 'ε', operand.vertexes[0] + last_vertex));
-        let op_final_state = *self.vertexes.last().unwrap();
-        // add final state
-        let nfa_final_state = op_final_state + 1;
-        self.vertexes.push(nfa_final_state);
-        self.transitions.push((self_final_state, 'ε', nfa_final_state));
-        self.transitions.push((op_final_state, 'ε', nfa_final_state));
     }
 }
 
